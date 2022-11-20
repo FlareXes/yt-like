ce15d300016c12f08f91626fcda95e0931cecfc7
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-11-20 19:33:28 +0530

    permission issue fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+chmod +x ./like
+
 sudo mkdir -p /opt/yt-like/
 
 sudo cp ./like.py /opt/yt-like/
@@ -9,8 +11,6 @@ sudo cp ./like /usr/local/bin/
 sudo cp ./requirements.txt /opt/yt-like/
 
 sudo chown -R $(whoami):$(whoami) /opt/yt-like/
-
-chmod +x /usr/local/bin/like
 
 cd /opt/yt-like/
 
